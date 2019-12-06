7100228ad7a3547362e6fe513c54a4213d2db6f1
Justin Foley <justinrfoley@yahoo.com>
2019-12-05 18:29:46 -0700

    Further refactoring

--- 2019/Justin/computer.py
+++ 2019/Justin/computer.py
@@ -10,56 +10,76 @@ def comp(compList, inputVal):
             modes = [int(x) for x in list(op[:-2])[::-1]]
         else:
             operation = int(op)
-        
-        if (operation == 1):
-            param1 = compList[compList[pos + 1]] if len(modes) < 1 or modes[0] == 0 else compList[pos + 1]
-            param2 = compList[compList[pos + 2]] if len(modes) < 2 or modes[1] == 0 else compList[pos + 2]
-            try:
-                compList[compList[pos+3]] = param1 + param2
-            except:
-                return [0]
-            pos+=4
-        elif (operation == 2):
-            param1 = compList[compList[pos + 1]] if len(modes) < 1 or modes[0] == 0 else compList[pos + 1]
-            param2 = compList[compList[pos + 2]] if len(modes) < 2 or modes[1] == 0 else compList[pos + 2]
-            try:
-                compList[compList[pos+3]] = param1 * param2
-            except:
-                return [0]
-            pos+=4
-        elif (operation == 3):
+
+        # No params
+        # Exit op
+        if (operation == 99):
+            return compList
+
+        # No getter, One setter
+        # takes a single integer as input and saves it to the position given by its only parameter
+        if (operation == 3):
             compList[compList[pos + 1]] = inputVal
             pos+=2
-        elif (operation == 4):
-            if len(modes) < 1 or modes[0] == 0:
-                print("Output: " + str(compList[compList[pos + 1]]))
-            elif modes[0] == 1:
-                print("Output: " + str(compList[pos+1]))
+            continue
+        
+        # Single getter, no setter
+        if len(modes) < 1 or modes[0] == 0:
+            param1 = compList[compList[pos+1]]
+        elif modes[0] == 1:
+            param1 = compList[pos+1]
+        
+        # outputs the value of its only parameter
+        if (operation == 4):
+            print("Output: " + str(param1))
             pos+=2
-        elif (operation == 5):
-            param1 = compList[compList[pos+1]] if len(modes) < 1 or modes[0] == 0 else compList[pos+1]
-            param2 = compList[compList[pos+2]] if len(modes) < 2 or modes[1] == 0 else compList[pos+2]
+            continue
+
+        # Two getters, no setter
+        if len(modes) < 2 or modes[1] == 0:
+            param2 = compList[compList[pos+2]]
+        elif modes[1] == 1:
+            param2 = compList[pos+2]
+
+        # jump-if-true: if the first parameter is non-zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
+        if (operation == 5):
             if param1 != 0:
                 pos = param2
             else:
                 pos+=3
-        elif (operation == 6):
-            param1 = compList[compList[pos+1]] if len(modes) < 1 or modes[0] == 0 else compList[pos+1]
-            param2 = compList[compList[pos+2]] if len(modes) < 2 or modes[1] == 0 else compList[pos+2]
+            continue
+
+        # jump-if-false: if the first parameter is zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
+        if (operation == 6):
             if param1 == 0:
                 pos = param2
             else:
                 pos+=3
-        elif (operation == 7):
-            param1 = compList[compList[pos + 1]] if len(modes) < 1 or modes[0] == 0 else compList[pos + 1]
-            param2 = compList[compList[pos + 2]] if len(modes) < 2 or modes[1] == 0 else compList[pos + 2]
+            continue
+        
+        # Two getters, one setter
+        # adds together numbers read from two positions and stores the result in a third position
+        if (operation == 1):
+            compList[compList[pos+3]] = param1 + param2
+            pos+=4
+            continue
+
+        # multiplies together numbers read from two positions and stores the result in a third position
+        if (operation == 2):
+            compList[compList[pos+3]] = param1 * param2
+            pos+=4
+            continue
+
+        # less than: if the first parameter is less than the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
+        if (operation == 7):
             compList[compList[pos+3]] = 1 if param1 < param2 else 0
             pos+=4
-        elif (operation == 8):
-            param1 = compList[compList[pos + 1]] if len(modes) < 1 or modes[0] == 0 else compList[pos + 1]
-            param2 = compList[compList[pos + 2]] if len(modes) < 2 or modes[1] == 0 else compList[pos + 2]
+            continue
+
+        # equals: if the first parameter is equal to the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
+        if (operation == 8):
             compList[compList[pos+3]] = 1 if param1 == param2 else 0
             pos+=4
-        elif (operation == 99):
-            return compList
+            continue
+        
     return compList
